69d77185030bf0ebeeb605d65272cb524be806ec
AmazingRoad <AmazingRoad@163.com>
2019-10-07 21:01:15 +0800

    save model every t seconds

--- train_neurodata.py
+++ train_neurodata.py
@@ -12,6 +12,7 @@ import torch.nn.functional as F
 import numpy as np
 from core.models.ffn import FFN
 from core.data import BatchCreator
+import time
 
 
 parser = argparse.ArgumentParser(description='Train a network.')
@@ -24,6 +25,8 @@ parser.add_argument('--input_size', default=(33, 33, 33), help='input size')
 parser.add_argument('--clip_grad_thr', type=float, default=0.7, help='grad clip threshold')
 parser.add_argument('--save_path', type=str, default='./model', help='model save path')
 parser.add_argument('--resume', type=str, default=None, help='resume training')
+parser.add_argument('--interval', type=int, default=120, help='How often to save model (in seconds).')
+parser.add_argument('--iter', type=int, default=1e100, help='training iteration')
 
 args = parser.parse_args()
 
@@ -56,50 +59,57 @@ def run():
     best_loss = np.inf
 
     """获取数据流"""
-    for iter, (seeds, images, labels, offsets) in enumerate(
-            get_batch(train_loader, args.batch_size, args.input_size,
-                      partial(fixed_offsets, fov_moves=train_dataset.shifts))):
+    t_last = time.time()
+    cnt = 0
+    while cnt < args.iter:
+        for iter, (seeds, images, labels, offsets) in enumerate(
+                get_batch(train_loader, args.batch_size, args.input_size,
+                          partial(fixed_offsets, fov_moves=train_dataset.shifts))):
 
-        pos_w = - torch.log((labels > 0.5).sum().float() / np.prod(labels.shape))
-        slice = seeds[:, :, seeds.shape[2] // 2, :, :].sigmoid()
-        seeds[:, :, seeds.shape[2] // 2, :, :] = slice
-        labels = labels.cuda()
+            cnt += 1
+            t_curr = time.time()
+            """正样本权重"""
+            pos_w = - torch.log((labels > 0.5).sum().float() / np.prod(labels.shape))
+            slice = seeds[:, :, seeds.shape[2] // 2, :, :].sigmoid()
+            seeds[:, :, seeds.shape[2] // 2, :, :] = slice
+            labels = labels.cuda()
 
-        input_data = torch.cat([images, seeds], dim=1)
-        input_data = Variable(input_data.cuda())
+            input_data = torch.cat([images, seeds], dim=1)
+            input_data = Variable(input_data.cuda())
 
-        logits = model(input_data)
-        updated = seeds.cuda() + logits
+            logits = model(input_data)
+            updated = seeds.cuda() + logits
 
-        optimizer.zero_grad()
-        loss = F.binary_cross_entropy_with_logits(updated, labels, pos_weight=pos_w)
-        loss.backward()
-        """梯度截断"""
-        torch.nn.utils.clip_grad_value_(model.parameters(), args.clip_grad_thr)
-        optimizer.step()
+            optimizer.zero_grad()
+            loss = F.binary_cross_entropy_with_logits(updated, labels, pos_weight=pos_w)
+            loss.backward()
+            """梯度截断"""
+            torch.nn.utils.clip_grad_value_(model.parameters(), args.clip_grad_thr)
+            optimizer.step()
 
-        # update_seed(updated, seeds, model, offsets)
-        seeds = updated
+            # update_seed(updated, seeds, model, offsets)
+            seeds = updated
 
-        pred_mask = (updated >= logit(0.9)).detach().cpu().numpy()
-        true_mask = (labels > 0.5).cpu().numpy()
-        true_bg = np.logical_not(true_mask)
-        pred_bg = np.logical_not(pred_mask)
-        tp = (true_mask & pred_mask).sum()
-        fp = (true_bg & pred_mask).sum()
-        fn = (true_mask & pred_bg).sum()
-        tn = (true_bg & pred_bg).sum()
-        precision = 1.0 * tp / max(tp + fp, 1)
-        recall = 1.0 * tp / max(tp + fn, 1)
-        accuracy = 1.0 * (tp + tn) / (tp + tn + fp + fn)
-        print("loss: {:.2f}, iteration: {}, Precision: {:.2f}%, Recall: {:.2f}%, Accuracy: {:.2f}%".format(
-            loss.item(), iter, precision*100, recall*100, accuracy * 100))
+            pred_mask = (updated >= logit(0.9)).detach().cpu().numpy()
+            true_mask = (labels > 0.5).cpu().numpy()
+            true_bg = np.logical_not(true_mask)
+            pred_bg = np.logical_not(pred_mask)
+            tp = (true_mask & pred_mask).sum()
+            fp = (true_bg & pred_mask).sum()
+            fn = (true_mask & pred_bg).sum()
+            tn = (true_bg & pred_bg).sum()
+            precision = 1.0 * tp / max(tp + fp, 1)
+            recall = 1.0 * tp / max(tp + fn, 1)
+            accuracy = 1.0 * (tp + tn) / (tp + tn + fp + fn)
+            print("loss: {:.2f}, iteration: {}, Precision: {:.2f}%, Recall: {:.2f}%, Accuracy: {:.2f}%".format(
+                loss.item(), iter, precision*100, recall*100, accuracy * 100))
 
-        """根据最佳loss并且保存模型"""
-        if best_loss > loss.item():
-            best_loss = loss.item()
-            torch.save(model.state_dict(), os.path.join(args.save_path, 'ffn.pth'))
-            print('Model saved!')
+            """根据最佳loss并且保存模型"""
+            if best_loss > loss.item() or t_curr - t_last > args.interval:
+                t_last = t_curr
+                best_loss = loss.item()
+                torch.save(model.state_dict(), os.path.join(args.save_path, 'ffn.pth'))
+                print('Model saved!')
 
 
 if __name__ == "__main__":
