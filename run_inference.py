b028461c616df7057ac7aff0deb881451deb8ed2
AmazingRoad <AmazingRoad@163.com>
2019-10-13 22:12:15 +0800

    modify inference

--- run_inference.py
+++ run_inference.py
@@ -1,20 +1,15 @@
 import os
-import sys
-import numpy as np
-import time
-from scipy.special import expit
-from scipy.special import logit
 import h5py
 import argparse
 from core.models.ffn import FFN
 from core.data.utils import *
 
 parser = argparse.ArgumentParser(description='inference script')
-parser.add_argument('--data', type=str, default='./images.h5', help='input images')
-parser.add_argument('--label', type=str, default='./groundtruth.h5', help='input images')
-parser.add_argument('--model', type=str, default='./model/ffn1.pth', help='path to ffn model')
-parser.add_argument('--delta', default=(8, 8, 8), help='delta offset')
-parser.add_argument('--input_size', default=(33, 33, 33), help='input size')
+parser.add_argument('--data', type=str, default='./data1.h5', help='input images')
+parser.add_argument('--label', type=str, default='./pred.h5', help='input images')
+parser.add_argument('--model', type=str, default='./model/ffn.pth', help='path to ffn model')
+parser.add_argument('--delta', default=(5, 5, 5), help='delta offset')
+parser.add_argument('--input_size', default=(31, 31, 31), help='input size')
 parser.add_argument('--seg_thr', type=float, default=0.6, help='input size')
 parser.add_argument('--mov_thr', type=float, default=0.9, help='input size')
 parser.add_argument('--act_thr', type=float, default=0.95, help='input size')
@@ -30,14 +25,19 @@ def run():
     model.load_state_dict(torch.load(args.model))
 
     with h5py.File(args.data, 'r') as f:
-        images = (f['raw'].value.astype(np.float32) - 128) / 33
+        images = (f['image'][()].astype(np.float32) - 128) / 33
         # labels = g['label'].value
 
     canva = Canvas(model, images, args.input_size, args.delta, args.seg_thr, args.mov_thr, args.act_thr)
     canva.segment_all()
+    result = canva.segmentation
+    max_value = result.max()
+    indice, count = np.unique(result, return_counts=True)
+    result[result == -1] = 0
+    result = result*(1.0 * 255/max_value)
     with h5py.File(args.label, 'w') as g:
-        f.create_dataset('raw', data=canva.segmentation, compression='gzip')
-    print('done')
+        g.create_dataset('raw', data=result.astype(np.uint8), compression='gzip')
+    print('label: {}, number: {}'.format(indice, count))
 
 
 if __name__ == '__main__':
